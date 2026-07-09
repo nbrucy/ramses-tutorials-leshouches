c28fda9dba7a0ce5ffe583dacf03c291fb273f1b
Noé Brucy <noe.brucy@ens-lyon.fr>
2026-07-09 08:38:14 +0200

    update job script

--- SN-Turbulence/job.sh
+++ SN-Turbulence/job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-#SBATCH --job-name=brucy_p3
-#SBATCH --ntasks=32
+#SBATCH --job-name=yourname_p3
+#SBATCH --ntasks=24
 #SBATCH --cpus-per-task=1
-#SBATCH --partition=Cascade
-##SBATCH --reservation=Houches2026
+#SBATCH --partition=Cascade-GPU
+#SBATCH --reservation=Houches2026
 
 mpirun ./ramses3d run.nml > out.${SLURM_JOB_ID}
